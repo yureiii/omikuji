5ff745498c7783e6ab0551ae49f04e081ec24bd8
Yureiii <ytd9260207@gmail.com>
2021-01-03 00:14:11 +0900

    README.md にあるリンクの綴りを間違えていたので直した. 床の変更時間が戻ってしまっていたので時間を変えた.

--- divination.js
+++ divination.js
@@ -16,7 +16,7 @@ var timeout;
 const floor = document.getElementById('floor');
 // カメラカーソルが床から離れると床が前方に出ながら大きくなり, 時間がたつと showOmikujiResult 関数を呼び出す
 omikuji.addEventListener('mouseleave', (e) => {
-  const enlargementTime = 2000;
+  const enlargementTime = 1700;
   // 移動先の設定が 300 300 300 となっているが, これをうまいこと変数で表したい
   floor.setAttribute('animation__increasingInSize', 'property: scale; dur: ' + enlargementTime + '; to: 300 300 300');
   floor.setAttribute('animation__moveUpward', 'property: position; dur: ' + enlargementTime + '; to: 0 10 0');
