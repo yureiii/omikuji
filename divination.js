59d180d2ad6f3ebdc5f62f3b5227719c7b651e58
Yureiii <32612544+yureiii@users.noreply.github.com>
2021-01-11 10:57:04 +0900

    スマートフォンで見るとモデルの位置がぶれて不安定になるので, mouseclick 用オブジェクト作成

--- divination.js
+++ divination.js
@@ -1,7 +1,9 @@
+const omikujiCollision = document.getElementById('omikujiCollision');
 const omikuji = document.getElementById('omikuji');
 var audio = new Audio("seund/tm2_can004.wav");
 // カメラカーソルが物体を振れている状態でクリックをすると音を立てて回りだす
-omikuji.addEventListener('mouseenter', (e) => {
+omikujiCollision.addEventListener('mouseenter', (e) => {
+  console.log("mouseenter");
   omikuji.setAttribute('animation', 'property: rotation; to: 0 360 0; loop: true; dur: 3000; easing: linear;');
   audio = new Audio();
   audio.src = "sound/tm2_can004.wav";
@@ -15,11 +17,12 @@ omikuji.addEventListener('mouseenter', (e) => {
 var timeout;
 const floor = document.getElementById('floor');
 // カメラカーソルが床から離れると床が前方に出ながら大きくなり, 時間がたつと showOmikujiResult 関数を呼び出す
-omikuji.addEventListener('mouseleave', (e) => {
+omikujiCollision.addEventListener('mouseleave', (e) => {
+  console.log("mouseleave");
   const enlargementTime = 1700;
   // 移動先の設定が 300 300 300 となっているが, これをうまいこと変数で表したい
   floor.setAttribute('animation__increasingInSize', 'property: scale; dur: ' + enlargementTime + '; to: 300 300 300');
-  floor.setAttribute('animation__moveUpward', 'property: position; dur: ' + enlargementTime + '; to: 0 10 0');
+  floor.setAttribute('animation__moveUpward', 'property: position; dur: ' + enlargementTime + '; to: 0 0 0');
   audio.pause();
   timeout = window.setTimeout(showOmikujiResult, enlargementTime);
 });
